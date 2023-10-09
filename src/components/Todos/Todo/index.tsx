c307118df009539cfb7a608bade3a148d620ce97
Stalmax76 <68605477+Stalmax76@users.noreply.github.com>
2023-10-09 20:07:44 +0200

    added todoIcon in Todo-component

--- src/components/Todos/Todo/index.tsx
+++ src/components/Todos/Todo/index.tsx
@@ -1,8 +1,10 @@
+import { RiTodoFill } from 'react-icons/ri';
 import styles from './style.module.scss';
 
 function Todo({ todo, deleteTodo }: { todo: string; deleteTodo: () => void }) {
   return (
     <div className={styles.todo} onDoubleClick={deleteTodo}>
+      <RiTodoFill className={styles.todoIcon} />
       <div className={styles.todoText}> {todo}</div>
     </div>
   );
